f56f2ec51e54479dfbf27dd3ee00d361d4c6d7bf
inzaghi123456789 <u@d.tld>
2012-08-18 11:50:14 +0200

    * partial support for kernels 3.3.x and above (the analog tuner only)

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ avermedia-a828
 patched avermedia a828 hybrid tuner for newer kernels
 
 * support for kernels up to 3.2.x
+* partial support for kernels 3.3.x and above (the analog tuner only)
 
 notes::
 * teletext support might have been crippled
 * stability issues might have been introduced
+* the dvbt tuner will not work correctly for kernels 3.3.x and above
 
--- installer/src-blessed-x64/aver/osdep_dvb.c
+++ installer/src-blessed-x64/aver/osdep_dvb.c
@@ -168,10 +168,12 @@ static int dvb_fe_ioctl(struct dvb_frontend *fe, unsigned int cmd, void *arg);
 #endif
 static int demux_sw_start_feed(struct dvb_demux_feed *feed);
 static int demux_sw_stop_feed(struct dvb_demux_feed *feed);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 static int dvbfe_set_parameters(struct dvb_frontend *fe,
 			struct dvb_frontend_parameters *param);
 static int dvbfe_get_parameters(struct dvb_frontend *fe,
 			struct dvb_frontend_parameters *param);
+#endif
 static int dvbfe_read_status(struct dvb_frontend* fe, fe_status_t* status);
 static int dvbfe_read_ber(struct dvb_frontend* fe, u32* ber);
 static int dvbfe_read_signal_strength(struct dvb_frontend* fe, u16* strength);
@@ -276,8 +278,13 @@ int SysDVBRegisterFrontend(dvb_t dev, struct dvb_frontend_info *info,
 
 	p->fe.ops.info = *info;
 	p->fe.ops.release = dvbfe_release;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 	p->fe.ops.set_frontend = dvbfe_set_parameters;
 	p->fe.ops.get_frontend = dvbfe_get_parameters;
+#else
+	p->fe.ops.set_frontend = NULL;
+	p->fe.ops.get_frontend = NULL;
+#endif
 	p->fe.ops.read_status = dvbfe_read_status;
 	p->fe.ops.read_ber = dvbfe_read_ber;
 	p->fe.ops.read_signal_strength =
@@ -753,6 +760,7 @@ static int demux_sw_stop_feed(struct dvb_demux_feed *feed)
 	return status;
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 static int dvbfe_set_parameters(struct dvb_frontend *fe,
 				struct dvb_frontend_parameters *param)
 {
@@ -760,7 +768,9 @@ static int dvbfe_set_parameters(struct dvb_frontend *fe,
 	DBG_fCDVB("calling SetParameters()\n");
 	return SetParameters(p->context, param);
 }
+#endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 static int dvbfe_get_parameters(struct dvb_frontend *fe,
 				struct dvb_frontend_parameters *param)
 {
@@ -768,6 +778,7 @@ static int dvbfe_get_parameters(struct dvb_frontend *fe,
 	DBG_fCDVB("calling GetParameters()\n");
 	return GetParameters(p->context, param);
 }
+#endif
 
 static int dvbfe_read_status(struct dvb_frontend* fe, fe_status_t* status)
 {
@@ -929,12 +940,20 @@ static int dvb_fe_ioctl(struct dvb_frontend *fe, unsigned int cmd, void *arg)
 		return dvbfe_read_snr(fe, (u16 *)arg);
 
 	case FE_SET_FRONTEND:
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 		return dvbfe_set_parameters(fe,
 					(struct dvb_frontend_parameters *)arg);
+#else
+		return -EINVAL;
+#endif
 	
 	case FE_GET_FRONTEND:
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
 		return dvbfe_get_parameters(fe,
 					(struct dvb_frontend_parameters *)arg);
+#else
+		return -EINVAL;
+#endif
 
 	case FE_INIT:
 		return dvbfe_init(fe);
